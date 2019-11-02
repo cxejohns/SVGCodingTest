6d0c537cc44e0192685fffa98455048538715a64
root <root@DESKTOP-O2QDCGO.localdomain>
2019-11-02 15:59:31 -0400

    Created initial MVC solution file, along with Home Controller, Index View, and initial models and Data Access Layer

---
+++ SVGCodingTest/SVGCodingTest/DAL/ISaleDAO.cs
@@ -0,0 +1,24 @@
+﻿using SVGCodingTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVGCodingTest.DAL
+{
+    public interface ISaleDAO
+    {
+        /// <summary>
+        /// Gets a single recipe
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Sale GetSale(int id);
+
+        /// <summary>
+        /// Returns all of the recipes
+        /// </summary>
+        /// <returns></returns>
+        IList<Sale> GetSales();
+    }
+}
---
+++ SVGCodingTest/SVGCodingTest/Models/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SVGCodingTest.Models
+{
+    public class Vehicle 
+    {
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        // add other vehicle data here
+
+    }
+}
