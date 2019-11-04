9dce96c5d1b1b2b732160ac5a0553394ab4beda7
root <root@DESKTOP-O2QDCGO.localdomain>
2019-11-04 12:35:35 -0500

    installed telerik UI library and created a new solution file to test SVG export (successful)

--- SVGCodingTest/SVGCodingTest/DAL/IVehicleDAO.cs
+++ SVGCodingTest/SVGCodingTest/DAL/IVehicleDAO.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using SVGCodingTest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace SVGCodingTest.DAL
 {
-    public class IVehicleDAO
+    public interface IVehicleDAO
     {
+        /// <summary>
+        /// Gets a single recipe
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        Vehicle GetVehicle(int id);
+
+        /// <summary>
+        /// Returns all of the recipes
+        /// </summary>
+        /// <returns></returns>
+        IList<Vehicle> GetVehicles();
     }
 }
---
+++ dupe/SVGCodingTest/SVGCodingTest/App_Start/BarChartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SVGCodingTest.App_Start
+{
+    public class BarChartController : Controller
+    {
+        // GET: BarChart
+        public ActionResult DrawBarChart()
+        {
+            return View();
+        }
+    }
+}
